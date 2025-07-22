9129e8a5a946693745324b41e06dd0acd4dfac30
Itachi Shrestha <107382870+TechMangaOfficial@users.noreply.github.com>
2025-07-22 20:02:35 +0545

    Updating

--- script.js
+++
@@ -1,5 +0,0 @@
-// Make the heading blink every 700ms
-setInterval(() => {
-  const h1 = document.querySelector('.blinking');
-  h1.style.visibility = h1.style.visibility === 'hidden' ? 'visible' : 'hidden';
-}, 700);
